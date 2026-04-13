83f475e0c6011348d1f15428ef94140bd8deae01
panlisac <pan.lisac@gmail.com>
2026-04-13 12:28:19 -0400

    Initial final project file

---
+++ Final Project/Final Project/Final_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Final_ProjectApp.swift
+//  Final Project
+//
+//  Created by Lisa Pan on 4/13/26.
+//
+
+import SwiftUI
+
+@main
+struct Final_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Final Project/Final ProjectTests/Final_ProjectTests.swift
@@ -0,0 +1,17 @@
+//
+//  Final_ProjectTests.swift
+//  Final ProjectTests
+//
+//  Created by Lisa Pan on 4/13/26.
+//
+
+import Testing
+@testable import Final_Project
+
+struct Final_ProjectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
